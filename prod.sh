4f43bb691cd0015d9935a971f566eea57a7517b3
k-Richie <126113214+k-Richie@users.noreply.github.com>
2023-05-26 09:54:06 +0530

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -5,9 +5,9 @@ echo "Wait........."
 if [ -d "$folder_name" ]; then
   cd "$folder_name"
   if [ -f "dev.json" ]; then
-    aws cloudformation deploy --template-file cfm.yaml --parameter-overrides file://dev.json  --stack-name "$folder_name" --capabilities CAPABILITY_NAMED_IAM --region us-east-1
+    aws cloudformation deploy --template-file cfn.yaml --parameter-overrides file://dev.json  --stack-name "$folder_name" --capabilities CAPABILITY_NAMED_IAM --region us-east-1
   else
-    aws cloudformation deploy --template-file cfm.yaml --stack-name "$folder_name" --capabilities CAPABILITY_NAMED_IAM --region us-east-1
+    aws cloudformation deploy --template-file cfn.yaml --stack-name "$folder_name" --capabilities CAPABILITY_NAMED_IAM --region us-east-1
   fi
 else
   echo "Folder not found: $folder_name"
